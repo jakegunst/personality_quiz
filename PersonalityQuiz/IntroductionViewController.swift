f06a31141a636840ad7848d288da959ab8b79772
Jake Gunst <jacobwgunst@gmail.com>
2019-03-07 19:36:06 -0600

    Added navigation features.

--- PersonalityQuiz/IntroductionViewController.swift
+++ PersonalityQuiz/IntroductionViewController.swift
@@ -8,15 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+var fur : Int = 0
+var meat : Int = 0
+var friendly : Double = 0.0
 
+
+class ViewController: UIViewController {
+    @IBAction func furCheck(_ sender: Any) {
+        
+    }
+    @IBAction func meatCheck(_ sender: Any) {
+        
+    }
+    @IBAction func friendlyCheck(_ sender: Any) {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         print("test 2.")
     }
-
 
 }
 
